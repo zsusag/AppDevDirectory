3911c0b5da56455801b1ddc33188801ec022d242
zsusag <zsusag@gmail.com>
2016-02-12 19:42:36 -0600

    Initial Upload

---
+++ app/src/main/java/edu/appdev/appdevdirectory/MainActivity.java
@@ -0,0 +1,35 @@
+package edu.appdev.appdevdirectory;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mSwitchActivityButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mSwitchActivityButton=(Button) findViewById(R.id.switchActivityButton);
+        final Intent intent = new Intent(this, ProfileActivity.class);
+        mSwitchActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+
+            }
+
+        });
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }}
