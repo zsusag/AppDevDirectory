40ea7ad8c98d0235c681db48b14fe65df07f27b2
Marcel Champagne <voiceofmarcel@gmail.com>
2016-02-21 14:35:46 -0600

    Added threaded download

--- app/src/main/java/edu/appdev/appdevdirectory/MainActivity.java
+++ app/src/main/java/edu/appdev/appdevdirectory/MainActivity.java
@@ -1,22 +1,41 @@
 package edu.appdev.appdevdirectory;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.Button;
 import android.widget.ListView;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class MainActivity extends AppCompatActivity {
+
+    String getStringFromURL(String url) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // JSON
+        new DownloadFilesTask().execute();
 
         // listView and ProfileAdapter adapters
         List profiles = new ArrayList();
@@ -30,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
         // View list item
         final Intent intent = new Intent(this, ProfileActivity.class);
         profilesView.setOnItemClickListener(
-                new AdapterView.OnItemClickListener(){
+                new AdapterView.OnItemClickListener() {
                     @Override
                     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                         startActivity(intent);
@@ -38,5 +57,27 @@ public class MainActivity extends AppCompatActivity {
                 }
         );
 
+    }
 
-    }}
+    // Download URL
+    class DownloadFilesTask extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... params) {
+            String s = "";
+            try {
+                s = getStringFromURL("http://www.cs.grinnell.edu/~pradhanp/android.json");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return s;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            System.out.println(result);
+        }
+    }
+
+
+}
