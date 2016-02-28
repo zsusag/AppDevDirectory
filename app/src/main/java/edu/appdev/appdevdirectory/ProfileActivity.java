c0ba7dd43dc4fb4a4ec94d0401f57eed66c671ea
Zachary Susag <zsusag@gmail.com>
2016-02-28 14:37:38 -0600

    Add dynamic data for ProfileActivity

--- app/src/main/java/edu/appdev/appdevdirectory/ProfileActivity.java
+++ app/src/main/java/edu/appdev/appdevdirectory/ProfileActivity.java
@@ -5,12 +5,20 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.HashMap;
 
 public class ProfileActivity extends AppCompatActivity {
 
     private Button mBackButton;
+    private TextView mName;
+    private TextView mRole;
+    private TextView mYear;
+    private ImageView mPic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +28,17 @@ public class ProfileActivity extends AppCompatActivity {
 
         // Dynamic data
         HashMap<String, String> member = (HashMap) getIntent().getSerializableExtra("member");
+        mName = (TextView) findViewById(R.id.profNameTextView);
+        mRole = (TextView) findViewById(R.id.profRoleTextView);
+        mYear = (TextView) findViewById(R.id.profYearTextView);
+        mPic = (ImageView) findViewById(R.id.profImageView);
+
+        mName.setText(member.get("name"));
+        mRole.setText(member.get("role"));
+        mYear.setText(member.get("year"));
+        String url = member.get("image");
+        Picasso.with(getApplicationContext()).load(url).into(mPic);
+
 
         // Initialize intent
         final Intent intent = new Intent(this, ProfileActivity.class);
